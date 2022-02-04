94cf4df161b35eeb6d1c65b0c0d9edd651dd5b93
MAHAMADASIF ANSARI (maan) <maan@odoo.com>
2022-02-04 16:52:49 +0530

    [IMP] Improve code and add todoList feature

--- test.js
+++ test.js
@@ -1,18 +1,24 @@
 class abc {
     constructor() {
         this.messages = [];
+        this.todoList = [];
     }
     addMessage(message) {
         this.messages.push(message);
     }
     showMessage() {
         this.messages.forEach(message => {
-            console.log("Message is :: ", message);
+            console.log(message);
+        });
+    }
+    showTodoList() {
+        this.todoList.forEach(todo => {
+            console.log(todo);
         });
     }
 }
 
 (function () {
-    const abcInst = new abc("Hello World");
+    const abcInst = new abc();
     abcInst.showMessage();
 })();
